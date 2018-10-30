93ab9090ff775dd1758ea00167343398b6fafe58
Erik Landvall <erik@landvall.se>
2018-10-30 19:52:15 +0100

    Bugfix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const
-Client = require('./client'),
-Server = require('./server')
+Client = require('./src/client'),
+Server = require('./src/server')
 
 module.exports = Object.feeze({ Client, Server })
